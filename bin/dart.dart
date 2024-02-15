6556d09d4dede1cf4db8050e4725c8fc5a544c5b
porag.dev <mohidurrahman9078@gmail.com>
2024-02-15 22:47:23 +0600

    Initial commit

---
+++ bin/dart.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<String> studentList =['mohidur','karim','shafiq','rahim'];
+  print(studentList);
+  print(studentList.length);
+  studentList.add('Shamim');
+  print(studentList);
+  print(studentList[1]);
+
+}
